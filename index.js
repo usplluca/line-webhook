344879192ab45dc4b205a2963d10516d74a366ce
usplluca <usplkazuya04@gmail.com>
2025-07-04 14:54:12 +0900

    Update index.js

--- index.js
+++ index.js
@@ -28,42 +28,6 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  const echo = { type: 'text', text: `LUCA: ${event.message.text}` };
-  return client.replyMessage(event.replyToken, echo);
-}
-
-app.get('/', (req, res) => {
-  res.send('LUCA webhook is alive');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-const lineSdk = require('@line/bot-sdk');
-
-// 環境変数から取得
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-const client = new lineSdk.Client(config);
-
-app.post('/webhook', (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then((result) => res.json(result))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).end();
-    });
-});
-
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
-
   const userMessage = event.message.text;
 
   let replyText = "LUCA：なんて言えばいいか、ちょっと考えてる";
@@ -81,3 +45,11 @@ function handleEvent(event) {
     text: replyText
   });
 }
+
+app.get('/', (req, res) => {
+  res.send('LUCA webhook is alive');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
