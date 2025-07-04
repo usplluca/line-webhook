1aab3f1fe484f0a94b894c66d6b02c8e16d50e5a
usplluca <usplkazuya04@gmail.com>
2025-07-04 12:29:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
 const express = require('express');
-const line = require('@line/bot-sdk');
-
-const config = {
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET
-};
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
-app.use(line.middleware(config));
 
 app.post('/webhook', (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result));
+  console.log('Webhook received!', req.body);
+  res.send('OK');
 });
 
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    return Promise.resolve(null);
-  }
+app.get('/', (req, res) => {
+  res.send('LUCA webhook is alive');
+});
 
-  const client = new line.Client(config);
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: `LUCAからの返答：${event.message.text}`
-  });
-}
-
-app.listen(3000, () => {
-  console.log('LINE bot is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
